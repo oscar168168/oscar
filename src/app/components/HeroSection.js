8f96bab033b97ffcb556c1203caaf0fc0f84dcb9
DoublePainz <ratraksmey4@gmail.com>
2024-02-03 11:58:53 +0700

    all

--- src/app/components/HeroSection.js
+++ src/app/components/HeroSection.js
@@ -3,7 +3,7 @@ import { HeroContent } from "./HeroContent";
 
 export const HeroSection = () => {
   return (
-    <div className="lg:min-h-80 mt-20 lg:mt-0 flex justify-center items-center py-4 lg:py-20">
+    <div className="lg:min-h-80 mt-20 lg:mt-0 flex justify-center items-center py-10 lg:py-20">
       <HeroContent />
     </div>
   );
